be011ad6cf5152ef53503ed263ddc37524669d57
ArtemSv07 <135955009+ArtemSv07@users.noreply.github.com>
2023-12-15 11:20:55 +0200

    hw-6

---
+++ css/task-3.js
@@ -0,0 +1,33 @@
+'use strict';
+
+class StringBuilder {
+
+#value;
+
+constructor (value) {
+    this.#value = [value];
+}
+getValue() {
+    return this.#value.join('');
+}
+padEnd(str) {
+    this.#value.push(str);
+}
+padStart(str) {
+    this.#value.unshift(str);
+}
+padBoth(str) {
+    this.#value.unshift(str);
+    this.#value.push(str);
+}
+
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
